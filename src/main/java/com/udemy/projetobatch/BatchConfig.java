dd9b1dd45832c6b5ebb41154922a2a4a05419a2e
Pedro <ivo.mrqs@gmail.com>
2021-09-12 21:22:31 -0300

    projeto curso

---
+++ src/main/java/com/udemy/projetobatch/BatchConfig.java
@@ -0,0 +1,58 @@
+package com.udemy.projetobatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author pedro
+ *
+ *  @EnableBatchProcessing habilita o uso de componentes do framework spring batch
+ */
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job imprimirOlaJob() {
+		return jobBuilderFactory.get("imprimeOlaJob")
+				.start(imprimeOlaStep())
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+
+	private Step imprimeOlaStep() {
+
+		return stepBuilderFactory.get("imprimeOlaStep").tasklet(imprimeOlaTasklet(null)).build();
+	}
+
+	@Bean
+	@StepScope
+	public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println(String.format("Olá, %s!", nome));
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
+}
